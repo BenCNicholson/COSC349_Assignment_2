e9e65a44df385e1d71190f99f669127a958cb101
nicbe305 <nicbe305@student.otago.ac.nz>
2024-10-10 01:07:54 +1300

    changed webserve index

--- webServer/index.php
+++ webServer/index.php
@@ -18,11 +18,43 @@
         }
     </style>
 </head>
-<>
-<h1> 
-<?php
-include("../dbconnect.php");
+<body>
+<div class = "login-form">
+<form method="POST" action="authenticate.php">
+    <input type="text" name="username" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <input type="submit" value="Login">
+</form>
+<?php 
+include("dbconnect.php"); 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $stmt = $mysqli->prepare("SELECT _password, password FROM Client WHERE email = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->store_result();
+    if($stmt->num_rows>0){
+
+        $stmt->bind_result($pw);
+        $stmt->fetch();
+        if($password ==$pw){
+            setcookie("user_id", $username, time() + (86400 * 30), "/");
+            header("Location: dashboard.php");
+            exit();
+        }
+    }else{
+        echo "No user found with that username.";
+    }
+}
 ?>
-</h1>
+
+</div>
+
+
+<p><a href = "CreateUsr.php">Create an account</a></p>
+<p><href></href></p>
+<p><href></href></p>
+<p><href></href></p>
 </body>
 </html>
