dd3881d554583a9910e3ab6eed87d91b1b372ac4
nicbe305 <nicbe305@student.otago.ac.nz>
2024-10-10 07:19:23 +1300

    added elastic ips to dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php 
 #Definitions, will need to input the ip address after terraform creation
 define('MYSQL_SERVER_IP','sql-server.cvo4ama5v09p.us-east-1.rds.amazonaws.com');
-define('ADMIN_SERVER_IP','54.198.37.96');
-define('WEB_SERVER_IP','54.224.121.104');
+define('ADMIN_SERVER_IP','52.54.133.98');
+define('WEB_SERVER_IP','52.54.133.98');
 #Connect to db
 $db_host = MYSQL_SERVER_IP;
 $db_name = 'roomDB';
